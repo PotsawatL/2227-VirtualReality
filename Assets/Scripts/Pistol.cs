9e62f04be48ddc9c14efe342eb1872592f3d16e4
Potsawat Lalitlakkanakul <potsawatlalitlakkanakul@kkumail.com>
2023-03-27 12:41:05 +0700

    updated files

---
+++ Assets/Scripts/Pistol.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+public class Pistol : Weapon
+{
+    [SerializeField] private Projectile bulletPrefab;
+    [SerializeField] private AudioSource audioSource;
+
+    protected override void StartShooting(XRBaseInteractor interactor)
+    {
+        base.StartShooting(interactor);
+        Shoot();
+        audioSource.Play();
+    }
+
+    protected override void Shoot()
+    {
+        base.Shoot();
+        Projectile projectileInstance = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+        projectileInstance.Init(this);
+        projectileInstance.Launch();
+    }
+
+    protected override void StopShooting(XRBaseInteractor interactor)
+    {
+        base.StopShooting(interactor);
+    }
+}
---
+++ Assets/Scripts/XRGrabInteractableTwoAttach.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRGrabInteractableTwoAttach : XRGrabInteractable
+{
+    [SerializeField] Transform _leftAttachTransform;
+    [SerializeField] Transform _rightAttachTransform;
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        if (args.interactorObject.transform.CompareTag("LeftHand"))
+        {
+            attachTransform = _leftAttachTransform;
+        }
+
+        else if (args.interactorObject.transform.CompareTag("RightHand"))
+        {
+            attachTransform = _rightAttachTransform;
+        }
+
+        base.OnSelectEntered(args);
+    }
+}
